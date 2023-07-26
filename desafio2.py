4923cb95cf27b2c696db510c1b431f841c070eb4
fkamag <fabiokamagian@gmail.com>
2023-07-26 19:25:45 -0300

    incluindo cadastro de conta e vínculo ao usuário

--- desafio2.py
+++ desafio2.py
@@ -1,3 +1,5 @@
+import os
+
 user_list = [{
     'cpf': "admin",
     'name': "Admin",
@@ -9,11 +11,17 @@ user_list = [{
         'cidade': "Tatuí",
         'estado': "SP"
     },
-    'password': "123456",
+    'password': "123456"
+    }, ]
+
+account_list = [{
+    'agencia': '0001',
+    'numero_conta': '1',
+    'cpf': 'admin',
     'saque_diario': 0,
     'extrato': "",
     'saldo': 0
-    }, ]
+}, ]
 
 
 def menu():
@@ -34,9 +42,24 @@ def login():
     return cpf
 
 
-def buscar_usuario(user, user_list):
+def get_user(user, user_list):
     result = [usuario for usuario in user_list if usuario["cpf"] == user]
     return result[0] if result else None
+
+
+def get_account(user, account_list):
+    result = [account for account in account_list if account["cpf"] == user]
+    if len(result) == 1:
+        return result[0]
+    elif len(result) > 1:
+        print("Lista de Contas do Usuário")
+        for account in result:
+            print("Conta nº ", account["numero_conta"])
+        option = input("Digite o número da conta que deseja acessar: ")
+        selected = [cont for cont in result if cont["numero_conta"] == option]
+        return selected[0] if selected else None
+    else:
+        return None
 
 
 def valid_password(user):
@@ -51,6 +74,7 @@ def create_user(cpf):
     new_user = {}
     name = input("Nome: ")
     date = input("Data de Nascimento: ")
+    print("Endereço: ")
     logradouro = input("Logradouro: ")
     numero = input("Numero: ")
     bairro = input("Bairro: ")
@@ -61,16 +85,30 @@ def create_user(cpf):
         "cpf": cpf,
         "name": name,
         "data_nascimento": date,
-        "logradouro": logradouro,
-        "numero": numero,
-        "bairro": bairro,
-        "cidade": cidade,
-        "estado": estado,
+        "endereco": {
+            "logradouro": logradouro,
+            "numero": numero,
+            "bairro": bairro,
+            "cidade": cidade,
+            "estado": estado,
+        },
         "password": password,
-        "saque_diario": 0,
-        "extrato": "",
-        "saldo": 0})
+    })
     return new_user
+
+
+def create_account(user):
+    new_account = {}
+    number_account = len(account_list) + 1
+    new_account.update({
+        'agencia': '0001',
+        'numero_conta': str(number_account),
+        'cpf': user["cpf"],
+        'saque_diario': 0,
+        'extrato': "",
+        'saldo': 0
+    })
+    return new_account
 
 
 def menu_user():
@@ -85,13 +123,54 @@ escolha a opção desejada => """
     return input(menu)
 
 
+def deposit(saldo, extrato, user):
+    deposito = float(input("Informe o valor do depósito: "))
+    if deposito > 0:
+        saldo += deposito
+        extrato += f"depósito R$ {deposito:.2f} \n"
+        print(f"depósito R$ {deposito:.2f} realizado! \n")
+        user["saldo"] = saldo
+        user["extrato"] = extrato
+    else:
+        print("Valor de depósito inválido")
+
+
+def withdraw(saque_diario, saldo, extrato, user):
+    if saque_diario < 3:
+        saque = float(input("Informe o valor do saque: "))
+        if saque <= saldo:
+            if saque > 0:
+                saldo -= saque
+                extrato += f"saque R$ {saque:.2f} \n"
+                print(f"saque R$ {saque:.2f} realizado! \n")
+                saque_diario += 1
+                user["saldo"] = saldo
+                user["extrato"] = extrato
+                user["saque_diario"] = saque_diario
+            else:
+                print("Valor de saque inválido")
+        else:
+            print("Saldo insuficiente!")
+    else:
+        print("Ultrapassado limite de 3 saques diários")
+
+
+def statement(extrato, saldo):
+    if extrato:
+        print("Extrato de operações" + "\n")
+        print(extrato)
+        print(f"Saldo: R$ {saldo:.2f} \n")
+    else:
+        print("Não foram realizadas movimentações" + "\n")
+
+
 def operations_admin():
     while True:
         try:
             option = menu()
 
             if option == '1':
-                result = buscar_usuario(login(), user_list)
+                result = get_user(login(), user_list)
                 if result is None:
                     print("Usuário não encontrado")
                 else:
@@ -99,20 +178,36 @@ def operations_admin():
                     if valid is False:
                         print("senha inválida")
                     else:
-                        operations(result)
+                        user = result["cpf"]
+                        account = get_account(user, account_list)
+                        if account:
+                            print("Acessando Conta ", account["numero_conta"])
+                            operations(account)
+                        else:
+                            print("Cliente sem conta cadastrada")
 
             elif option == '2':
                 user = login()
-                result = buscar_usuario(user, user_list)
+                result = get_user(user, user_list)
                 if result:
                     print("CPF já cadastrado")
                 else:
                     new_user = create_user(user)
                     user_list.append(new_user)
-                    print("Cadastro realizado")
+                    print("Cadastro realizado com sucesso")
 
             elif option == '3':
-                print("Sistema de Criar Conta em Manutenção")
+                result = get_user(login(), user_list)
+                if result is None:
+                    print("Usuário não encontrado")
+                else:
+                    valid = valid_password(result)
+                    if valid is False:
+                        print("senha inválida")
+                    else:
+                        new_account = create_account(result)
+                        account_list.append(new_account)
+                        print("Conta cadastrada com sucesso")
             elif option == '4':
                 print("Encerrando sistema")
                 break
@@ -131,41 +226,14 @@ def operations(user):
             saque_diario = user["saque_diario"]
 
             if option == 'd':
-                deposito = float(input("Informe o valor do depósito: "))
-                if deposito > 0:
-                    saldo += deposito
-                    extrato += f"depósito R$ {deposito:.2f} \n"
-                    print(f"depósito R$ {deposito:.2f} realizado! \n")
-                    user["saldo"] = saldo
-                    user["extrato"] = extrato
-                else:
-                    print("Valor de depósito inválido")
+                deposit(saldo, extrato, user)
 
             elif option == 's':
-                if saque_diario < 3:
-                    saque = float(input("Informe o valor do saque: "))
-                    if saque <= saldo:
-                        if saque > 0:
-                            saldo -= saque
-                            extrato += f"saque R$ {saque:.2f} \n"
-                            print(f"saque R$ {saque:.2f} realizado! \n")
-                            saque_diario += 1
-                            user["saldo"] = saldo
-                            user["extrato"] = extrato
-                            user["saque_diario"] = saque_diario
-                        else:
-                            print("Valor de saque inválido")
-                    else:
-                        print("Saldo insuficiente!")
-                else:
-                    print("Ultrapassado limite de 3 saques diários")
+                withdraw(saque_diario, saldo, extrato, user)
+
             elif option == 'e':
-                if extrato:
-                    print("Extrato de operações" + "\n")
-                    print(extrato)
-                    print(f"Saldo: R$ {saldo:.2f} \n")
-                else:
-                    print("Não foram realizadas movimentações" + "\n")
+                statement(extrato, saldo)
+
             elif option == 'q':
                 print("Obrigado por utilizar nossos serviços!")
                 break
@@ -176,6 +244,7 @@ def operations(user):
 
 
 def main():
+    os.system('clear')
     operations_admin()
 
 
