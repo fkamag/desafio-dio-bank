19370e2f254da086da99219ed732eed18baa0e16
fkamag <fabiokamagian@gmail.com>
2023-07-25 21:30:35 -0300

    incluindo informações no readme e implementando o tratamento de erros

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # desafio-dio-bank
 Desafio do curso de Python da DIO
+
+O primeiro desafio está conforme os requisitos do pdf anexado ao projeto.
+
+O segundo desafio pede a criação de usuários e contas, sendo que os usuários são únicos,
+mas podem ter mais de uma conta.
+
+Usuário:
+composto por nome, data de nascimento, endereço no seguinte formato:
+logradouro, numero, bairro, cidade, estado, além do cpf.
+Não podemos cadastrar dois usuários com o mesmo CPF
+
+Conta Corrente:
+Agência com o número fixo 0001
+Uma conta pertecente a apenas um usuário, mas cada usuário pode ter mais de uma conta
+A numeração das contas são sequenciais, iniciando em 1.
+
+
--- desafio2.py
+++ desafio2.py
@@ -87,86 +87,92 @@ escolha a opção desejada => """
 
 def operations_admin():
     while True:
-        option = menu()
+        try:
+            option = menu()
 
-        if option == '1':
-            result = buscar_usuario(login(), user_list)
-            if result is None:
-                print("Usuário não encontrado")
-            else:
-                valid = valid_password(result)
-                if valid is False:
-                    print("senha inválida")
+            if option == '1':
+                result = buscar_usuario(login(), user_list)
+                if result is None:
+                    print("Usuário não encontrado")
                 else:
-                    operations(result)
+                    valid = valid_password(result)
+                    if valid is False:
+                        print("senha inválida")
+                    else:
+                        operations(result)
 
-        elif option == '2':
-            user = login()
-            result = buscar_usuario(user, user_list)
-            if result:
-                print("CPF já cadastrado")
+            elif option == '2':
+                user = login()
+                result = buscar_usuario(user, user_list)
+                if result:
+                    print("CPF já cadastrado")
+                else:
+                    new_user = create_user(user)
+                    user_list.append(new_user)
+                    print("Cadastro realizado")
+
+            elif option == '3':
+                print("Sistema de Criar Conta em Manutenção")
+            elif option == '4':
+                print("Encerrando sistema")
+                break
             else:
-                new_user = create_user(user)
-                user_list.append(new_user)
-                print("Cadastro realizado")
-
-        elif option == '3':
-            print("Sistema de Criar Conta em Manutenção")
-        elif option == '4':
-            print("Encerrando sistema")
-            break
-        else:
-            print("Opção inválida")
+                print("Opção inválida")
+        except ValueError:
+            print("Valor inválido, Digite um Número")
 
 
 def operations(user):
     while True:
-        option = menu_user()
-        saldo = user["saldo"]
-        extrato = user["extrato"]
-        saque_diario = user["saque_diario"]
+        try:
+            option = menu_user()
+            saldo = user["saldo"]
+            extrato = user["extrato"]
+            saque_diario = user["saque_diario"]
 
-        if option == 'd':
-            deposito = float(input("Informe o valor do depósito: "))
-            if deposito > 0:
-                saldo += deposito
-                extrato += f"depósito R$ {deposito:.2f} \n"
-                print(f"depósito R$ {deposito:.2f} realizado! \n")
-                user["saldo"] = saldo
-                user["extrato"] = extrato
-            else:
-                print("Valor de depósito inválido")
-
-        elif option == 's':
-            if saque_diario < 3:
-                saque = float(input("Informe o valor do saque: "))
-                if saque <= saldo:
-                    if saque > 0:
-                        saldo -= saque
-                        extrato += f"saque R$ {saque:.2f} \n"
-                        print(f"saque R$ {saque:.2f} realizado! \n")
-                        saque_diario += 1
-                        user["saldo"] = saldo
-                        user["extrato"] = extrato
-                        user["saque_diario"] = saque_diario
-                    else:
-                        print("Valor de saque inválido")
+            if option == 'd':
+                deposito = float(input("Informe o valor do depósito: "))
+                if deposito > 0:
+                    saldo += deposito
+                    extrato += f"depósito R$ {deposito:.2f} \n"
+                    print(f"depósito R$ {deposito:.2f} realizado! \n")
+                    user["saldo"] = saldo
+                    user["extrato"] = extrato
                 else:
-                    print("Saldo insuficiente!")
+                    print("Valor de depósito inválido")
+
+            elif option == 's':
+                if saque_diario < 3:
+                    saque = float(input("Informe o valor do saque: "))
+                    if saque <= saldo:
+                        if saque > 0:
+                            saldo -= saque
+                            extrato += f"saque R$ {saque:.2f} \n"
+                            print(f"saque R$ {saque:.2f} realizado! \n")
+                            saque_diario += 1
+                            user["saldo"] = saldo
+                            user["extrato"] = extrato
+                            user["saque_diario"] = saque_diario
+                        else:
+                            print("Valor de saque inválido")
+                    else:
+                        print("Saldo insuficiente!")
+                else:
+                    print("Ultrapassado limite de 3 saques diários")
+            elif option == 'e':
+                if extrato:
+                    print("Extrato de operações" + "\n")
+                    print(extrato)
+                    print(f"Saldo: R$ {saldo:.2f} \n")
+                else:
+                    print("Não foram realizadas movimentações" + "\n")
+            elif option == 'q':
+                print("Obrigado por utilizar nossos serviços!")
+                break
             else:
-                print("Ultrapassado limite de 3 saques diários")
-        elif option == 'e':
-            if extrato:
-                print("Extrato de operações" + "\n")
-                print(extrato)
-                print(f"Saldo: R$ {saldo:.2f} \n")
-            else:
-                print("Não foram realizadas movimentações" + "\n")
-        elif option == 'q':
-            print("Obrigado por utilizar nossos serviços!")
-            break
-        else:
-            print("Opção inválida")
+                print("Opção inválida")
+        except ValueError:
+            print("Valor inválido, Digite um Número")
 
 
 def main():
